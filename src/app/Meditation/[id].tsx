654f90764323d4315b26bd21f0184b7e2fcfca5f
shabin <85693363+5habin@users.noreply.github.com>
2025-01-17 12:32:18 +0400

    Update [id].tsx

--- src/app/Meditation/[id].tsx
+++ src/app/Meditation/[id].tsx
@@ -1,67 +1,110 @@
-import { Text, View, Pressable } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
-import { meditations } from "@/data";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { Link, router, useLocalSearchParams } from 'expo-router';
+import { Text, View, Pressable } from 'react-native';
+import { meditations } from '@/data';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import Slider from "@react-native-community/slider";
+import Slider from '@react-native-community/slider';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useAudioPlayer, useAudioPlayerStatus } from 'expo-audio';
+
 import audio from '@assets/meditations/audio1.mp3';
-import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
+import AnimatedBackground from '@/components/AnimatedBackground';
 
-export default function MeditationDetails(){
-    const {id} = useLocalSearchParams <{id : string }>();
+export default function MeditationDetails() {
+  const { id } = useLocalSearchParams<{ id: string }>();
 
-    const player = useAudioPlayer('@assets/meditations/audio1.mp3');
-    const status= useAudioPlayerStatus(player);
+  const player = useAudioPlayer(audio);
+  const status = useAudioPlayerStatus(player);
 
-    const meditation = meditations.find((m)=> m.id === Number(id));
-     if(!meditation){
-        return <text>Not found!</text>
-     }
-     return(
-     <SafeAreaView className="bg-orange-400 flex-1 p-2">
+  const meditation = meditations.find((m) => m.id === Number(id));
+
+  const formatSeconds = (milliseconds: number) => {
+    const minutes = Math.floor(milliseconds / 60000);
+    const seconds = Math.floor((milliseconds % 60000) / 1000);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  if (!meditation) {
+    return <Text>Meditation not found!</Text>;
+  }
+
+  return (
+    <SafeAreaView className="bg-orange-400 flex-1 p-2 justify-between">
+      <AnimatedBackground />
+      {/* Page content */}
       <View className="flex-1">
+        {/* Top part of the screen */}
         <View className="flex-1">
-            <View className="flex-row justify-between p-10 items-center">
-            <AntDesign name="infocirlceo" size={30} color="black"/>
-            <AntDesign
-              onPress={()=>router.back()}
-              name="close"
-              size={30}
-              color="black"
-              />
+          {/* Header */}
+          <View className="flex-row items-center justify-between p-10">
+            <AntDesign name="infocirlceo" size={24} color="black" />
+
+            <View className="bg-zinc-800 p-2 rounded-md">
+              <Text className="text-zinc-100 font-semibold">
+                Today's meditation
+              </Text>
             </View>
-            <Text className="text-3xl mt-10 text-center text-zinc-800 font-semibold">{meditation?.title}</Text>
+
+            <AntDesign
+              onPress={() => router.back()}
+              name="close"
+              size={26}
+              color="black"
+            />
+          </View>
+
+          <Text className="text-3xl mt-20 text-center text-zinc-800 font-semibold">
+            {meditation?.title}
+          </Text>
         </View>
 
-        <Pressable 
-         onPress={()=>player.playing ?player.pause() : player.play()}
-         className="bg-zinc-700 self-center w-24 aspect-square items-center justify-center rounded-full">
-        <FontAwesome6 name={status.playing ? 'pause' : 'play'} size={26} color="snow"/>
+        {/* Play/Pause Button */}
+        <Pressable
+          onPress={() => (player.playing ? player.pause() : player.play())}
+          className="bg-zinc-800 self-center w-20 aspect-square rounded-full items-center justify-center"
+        >
+          <FontAwesome6
+            name={status.playing ? 'pause' : 'play'}
+            size={24}
+            color="snow"
+          />
         </Pressable>
 
+        {/* Bottom part of the screen */}
         <View className="flex-1">
-          <View className="p-5 mt-auto">
+          {/* Footer: Player */}
+          <View className="p-5 mt-auto gap-5">
             <View className="flex-row justify-between">
-              <MaterialCommunityIcons name="cog-outline" size={24} color='#3A3937'/>
-              <MaterialIcons name="airplay" size={24} color='#3A3937'/>
+              <MaterialIcons name="airplay" size={24} color="#3A3937" />
+              <MaterialCommunityIcons
+                name="cog-outline"
+                size={24}
+                color="#3A3937"
+              />
             </View>
-
-            <View>
+            {/* Playback indicator */}
             <Slider
-              style={{width: '100%', height: 40}}
+              style={{ width: '100%', height: 3 }}
+              value={status.currentTime / status.duration}
+              onSlidingComplete={(value) =>
+                player.seekTo(value * status.duration)
+              }
               minimumValue={0}
               maximumValue={1}
-              minimumTrackTintColor="#3A3937"
               maximumTrackTintColor="#3A393755"
+              minimumTrackTintColor="#3A3937"
               thumbTintColor="#3A3937"
             />
+            {/* Times */}
+            <View className="flex-row justify-between">
+              <Text>{formatSeconds(status.currentTime)}</Text>
+              <Text>{formatSeconds(status.duration)}</Text>
             </View>
           </View>
         </View>
-       </View>
-     </SafeAreaView>
-     )
+      </View>
+    </SafeAreaView>
+  );
 }
